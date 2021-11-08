0500297ea49af11fdc0750e4ba57df48538064bf
Ryan Howard <ryanghoward@protonmail.com>
2021-11-08 13:32:14 -0800

    Link added to movieList data

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function MovieList(props) {
   return (
-    <div className="movie-list">
-      {props.movies.map(movie => (
+    <div className='movie-list'>
+      {props.movies.map((movie) => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
@@ -14,14 +15,16 @@ function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
 
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+    <Link>
+      <div className='movie-card'>
+        <h2>{title}</h2>
+        <div className='movie-director'>
+          Director: <em>{director}</em>
+        </div>
+        <div className='movie-metascore'>
+          Metascore: <strong>{metascore}</strong>
+        </div>
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
+    </Link>
   );
 }
