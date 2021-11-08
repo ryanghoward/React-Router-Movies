2885c066ab9dc547ef49554684947bfb4322ff41
Ryan Howard <ryanghoward@protonmail.com>
2021-11-08 13:06:59 -0800

    Movie info rendering

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ export default function App() {
       axios
         .get("http://localhost:5000/api/movies") // Study this endpoint with Postman
         .then((response) => {
+          setMovieList(response.data);
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
